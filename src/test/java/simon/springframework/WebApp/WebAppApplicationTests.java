97046977eb45f4c8aab9484724243ada522450d2
simon <smatusch@stud.hs-heilbronn.de>
2022-07-08 09:23:54 +0200

    Initial commit

---
+++ src/test/java/simon/springframework/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package simon.springframework.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
